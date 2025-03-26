11653a075db680d69dee6d855802c6061fb8fcbc
Shail1602 <143619265+Shail1602@users.noreply.github.com>
2025-03-26 13:01:12 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,16 @@ MODELS = ["mistral-large2", "llama3.1-70b", "llama3.1-8b"]
 
 connection_parameters = {
    
-    user=st.secrets["snowflake"]["SHAILESHSAUMYA"],
-    password=st.secrets["snowflake"]["Password@12ps!"],
-    account=st.secrets["snowflake"]["https://inxwylj-gi58960.snowflakecomputing.com"],  # Just "xy12345.region"
-    warehouse=st.secrets["snowflake"]["CORTEX_SEARCH_TUTORIAL_WH"],
-    database=st.secrets["snowflake"]["cortex_search_tutorial_db"],
-    schema=st.secrets["snowflake"]["PUBLIC"],
+  conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse=st.secrets["snowflake"]["warehouse"],
+    database=st.secrets["snowflake"]["database"],
+    schema=st.secrets["snowflake"]["schema"],
     role=st.secrets["snowflake"].get("role", "ACCOUNTADMIN")
+)
+
 
 }
 
