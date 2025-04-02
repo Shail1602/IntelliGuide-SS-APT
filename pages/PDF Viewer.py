bd45471aada702addc0c0b483e679fde020a4ac9
Shail1602 <143619265+Shail1602@users.noreply.github.com>
2025-04-03 04:54:36 +1100

    Update PDF Viewer.py

--- pages/PDF Viewer.py
+++ pages/PDF Viewer.py
@@ -187,8 +187,8 @@ if current_files:
                 """, unsafe_allow_html=True)
                 with open(file_path, "rb") as f:
                 col.markdown("<div class='center-btn'>", unsafe_allow_html=True)
-                st.download_button("📅 Download PDF", f, file_name=filename, key=f"dl_{filename}")
-                col.markdown("</div>", unsafe_allow_html=True)
-                st.markdown("</div></div>", unsafe_allow_html=True)
+                        st.download_button("📅 Download PDF", f, file_name=filename, key=f"dl_{filename}")
+                        col.markdown("</div>", unsafe_allow_html=True)
+                        st.markdown("</div></div>", unsafe_allow_html=True)
 else:
     st.warning("No PDF files match your search.")
