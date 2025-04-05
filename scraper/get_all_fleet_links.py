f4cb5bcaadc261582665705a37cc50c243e87a44
Shail1602 <143619265+Shail1602@users.noreply.github.com>
2025-04-05 21:48:55 +1100

    Update get_all_fleet_links.py

--- scraper/get_all_fleet_links.py
+++ scraper/get_all_fleet_links.py
@@ -15,11 +15,11 @@ soup = BeautifulSoup(sitemap_content, 'xml')
 urls = [loc.text for loc in soup.find_all('loc')]
 
 # Filter for tour detail pages
-tour_detail_pages = [url for url in urls if '/tours/' in url and url.count('/') > 6]
+fleet_detail_pages = [url for url in urls if '/our-fleet/' in url and url.count('/') > 5]
 
 # Save to a file or use as needed
-with open('scraper/tour_urls.txt', 'w') as f:
-    for url in tour_detail_pages:
+with open('scraper/fleets_urls.txt', 'w') as f:
+    for url in fleet_detail_pages:
         f.write(url + '\n')
 
-print(f"Extracted {len(tour_detail_pages)} tour detail pages.")
+print(f"Extracted {len(fleet_detail_pages)} fleet detail pages.")
