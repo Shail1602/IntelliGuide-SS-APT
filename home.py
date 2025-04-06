773a934e77d2a2fad2574cfa7abf40273a10b5dc
Shail1602 <143619265+Shail1602@users.noreply.github.com>
2025-04-06 15:49:52 +0000

    Faiss db

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.embeddings.base import Embeddings
 from sentence_transformers import SentenceTransformer
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pdfplumber
 
 class LocalSentenceEmbeddings(Embeddings):
     def __init__(self, model_path="local_model"):
