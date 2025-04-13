2c9d1a35875dc6c0ec6b0211be06dd855861f8aa
Shail1602 <143619265+Shail1602@users.noreply.github.com>
2025-04-13 05:33:03 +0000

    local_models/phi/models--microsoft--phi-1_5

--- home.py
+++ home.py
@@ -61,7 +61,7 @@ def extract_location_keywords(query):
 
 
 @st.cache_resource
-def get_local_llm(model_id="local_models/phi/models--microsoft--phi-1_5"):
+def get_local_llm(model_id="local_models/mistral7b"):
     from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
     print(f"📦 Attempting to load model from: {model_id}")
